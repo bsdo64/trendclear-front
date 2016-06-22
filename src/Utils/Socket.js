a8c798058ce0847940113a6b1ccd03213c70f569
ByeongSu Do <bsdo@naver.com>
2016-06-22 14:28:52 +0900

    add apiserver endpoint

--- src/Utils/Socket.js
+++ src/Utils/Socket.js
@@ -1,4 +1,10 @@
 const io = require('socket.io-client');
-const socket = io.connect('http://localhost:3001/noti');
+let socket;
+
+if (process.env.NODE_ENV === 'production') {
+  socket = io.connect('http://www.venacle.com:3001/noti');
+} else {
+  socket = io.connect('http://localhost:3001/noti');
+}
 
 module.exports = socket;
