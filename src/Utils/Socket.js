3f1fa68551887769a5a5fb9ff6d368a6b25166d9
ByeongSu Do <bsdo@naver.com>
2016-07-19 16:24:41 +0900

    fix socket.io endpoint

--- src/Utils/Socket.js
+++ src/Utils/Socket.js
@@ -2,7 +2,7 @@ const io = require('socket.io-client');
 
 let socket;
 if (process.env.NODE_ENV === 'production') {
-  socket = io.connect('http://venacle.com/noti');
+  socket = io.connect('http://io.venacle.com/noti');
 } else {
   socket = io.connect('http://localhost:3003/noti');
 }
