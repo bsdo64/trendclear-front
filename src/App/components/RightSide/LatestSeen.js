bde2be3a4ed98f99d61d23ca6c99d5c454d6a08c
ByeongSu Do <bsdo@naver.com>
2017-06-06 21:07:31 +0900

    add latestseen

---
+++ src/App/components/RightSide/LatestSeen.js
@@ -0,0 +1,89 @@
+/**
+ * Created by bsdo on 17. 3. 4.
+ */
+import React, {
+  Component,
+} from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import cx from 'classnames';
+import { getUser, getLatestSeenList } from '../../Selectors/User.js'
+import style from './index.css';
+
+class LatestSeen extends Component {
+  constructor(props) {
+    super(props);
+
+  }
+
+  render() {
+    const { user, latestSeenList } = this.props;
+
+    if (user) {
+      return (
+        <div className={cx([style.latestSeenList, style.widgetBox])}>
+          <div className={style.issueHeader}>
+            최근 본 포스트
+          </div>
+          <div className={style.issueWrap}>
+            <ol>
+              {
+                latestSeenList && latestSeenList.map((v, index) => {
+
+                  return (
+                    <li key={index}>
+                      <div className={cx([style.listItem])}>
+
+                        <Link to={`/club/${v.get('forum_id')}?postId=${v.get('id')}`}>
+                          <div className={style.issueKeyword}>
+                            {v.get('title')}
+                            <div className={style.meta}>
+                            <span className={style.type}>
+                              <i className="fa fa-heart-o" />{v.get('like_count')}
+                            </span>
+                              <span className={style.type}>
+                              <i className="fa fa-edit" />{v.get('comment_count')}
+                            </span>
+                            </div>
+                          </div>
+                        </Link>
+                      </div>
+                    </li>
+                  );
+                })
+              }
+            </ol>
+          </div>
+        </div>
+
+      )
+    } else {
+      return null;
+    }
+  }
+}
+
+LatestSeen.propTypes = {
+  latestSeenList: PropTypes.object,
+  user: PropTypes.object,
+};
+LatestSeen.defaultProps = {
+
+};
+
+const mapStateToProps = (state) => {
+  const StoreState = state.get('Stores');
+
+  return {
+    latestSeenList: getLatestSeenList(StoreState),
+    user: getUser(StoreState),
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  {
+
+  }
+)(LatestSeen);
